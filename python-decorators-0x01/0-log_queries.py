c6ab6fee3a14dc225724913a83e0a132de7b0712
Syriamme <roridaniel01@gmail.com>
2024-12-06 19:16:26 +0300

    decorators

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,15 +1,18 @@
 import sqlite3
+from datetime import datetime  # Import datetime for timestamps
 
-#### decorator to lof SQL queries
-
+# Decorator to log SQL queries with timestamps
 def log_queries(func):
-    def wrapper (*args, **kwargs):
+    def wrapper(*args, **kwargs):
+        # Get the current timestamp
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        # Log the SQL query (assumes it's the first argument)
         query = args[0] if args else None
         if query:
-            print(f"Query to be executed: {query}")
-        return func (*args, **kwargs)
+            print(f"[{timestamp}] Executing query: {query}")
+        # Execute the original function
+        return func(*args, **kwargs)
     return wrapper
-
 
 @log_queries
 def fetch_all_users(query):
@@ -20,5 +23,6 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-#### fetch users while logging the query
+# Fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
+print(users)
