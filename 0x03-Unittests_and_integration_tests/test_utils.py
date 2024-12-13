d7805db79e765107f79349bafb0d1f201ff3aa55
Syriamme <roridaniel01@gmail.com>
2024-12-14 00:20:04 +0300

    Unittests and Integration Tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,34 +1,26 @@
-#!/usr/bin/env python3
-"""
-Testing the access_nested_map function.
-"""
-
 import unittest
 from parameterized import parameterized
+from typing import Dict, Tuple, Union
 from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """
-    access_nested_map function test case.
+    Tests for the `access_nested_map` function.
     """
-
     @parameterized.expand([
-        ("test_simple_key", {"a": 1}, ("a",), 1),
-        ("test_nested_key", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("test_deep_nested_key", {"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, _, map_test, path, value):
-        """
-        Test the function returns the expected result.
+    def test_access_nested_map(
+        self,
+        nested_map: Dict,
+        path: Tuple[str],
+        expected: Union[Dict, int]
+    ) -> None:
 
-        Args:
-            _ (str): testname.
-            map_test (dict): Dictionary to test.
-            path (tuple): Key sequence.
-            value (Any): Expected result.
         """
-        self.assertEqual(access_nested_map(map_test, path), value)
-
-if __name__ == '__main__':
-    unittest.main()
+        Tests that `access_nested_map` returns the expected result.
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
