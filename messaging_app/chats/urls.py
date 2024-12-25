531c31ec31a9ced70153cdea4bb3e7c0c23e8d4e
Syriamme <roridaniel01@gmail.com>
2024-12-25 21:29:08 +0300

    messaging app project

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register the viewsets
-router = DefaultRouter()
+router = ["routers.DefaultRouter()"]
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
