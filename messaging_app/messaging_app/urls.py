ab038f9a05b003156892027e64a6089b6cfc740a
Syriamme <roridaniel01@gmail.com>
2024-12-25 21:52:56 +0300

    messaging app project

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/', include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
+
