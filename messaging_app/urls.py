257caf407d2a194924bf9b79f5c3edc02a91d07b
Syriamme <roridaniel01@gmail.com>
2024-12-25 21:41:41 +0300

    messaging app project

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/", include('chats.urls')),  # Add the chats app URLs under the /api/ prefix
+    path('api/', include('chats.urls')),  # Add the chats app URLs under the /api/ prefix
 ]
