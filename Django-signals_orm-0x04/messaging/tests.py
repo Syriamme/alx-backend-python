95a177075580aa5323e25c1c304ad122726e0d9e
Syriamme <roridaniel01@gmail.com>
2025-01-12 19:28:56 +0300

    messaging app

--- Django-signals_orm-0x04/messaging/tests.py
+++ Django-signals_orm-0x04/messaging/tests.py
@@ -46,16 +46,13 @@ class UserDeletionTest(TestCase):
         self.user1 = User.objects.create_user(username="user1", password="password")
         self.user2 = User.objects.create_user(username="user2", password="password")
 
-        # messages and notifications
         self.message = Message.objects.create(sender=self.user1, receiver=self.user2, content="Hello")
         self.notification = Notification.objects.create(user=self.user1, message="Notification")
         MessageHistory.objects.create(message=self.message, old_content="Original Content")
 
     def test_user_deletion_cleans_up_data(self):
-        # Deleting the user
         self.user1.delete()
 
-        # Checking related data is deleted
         self.assertFalse(Message.objects.filter(sender=self.user1).exists())
         self.assertFalse(Notification.objects.filter(user=self.user1).exists())
         self.assertFalse(MessageHistory.objects.filter(message__sender=self.user1).exists())
