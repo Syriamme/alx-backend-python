4a073e452870f989c503e2d9286d177f05f32492
Syriamme <roridaniel01@gmail.com>
2025-01-12 18:22:47 +0300

    messaging app

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,18 +2,48 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Message(models.Model):
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    sender = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sent_messages'
+    )
+    receiver = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='received_messages'
+    )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Message from {self.sender} to {self.receiver} at {self.timestamp}"
+        return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
+
 class Notification(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
-    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='notifications')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    message = models.ForeignKey(
+        Message,
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
     is_read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(
+        Message,
+        on_delete=models.CASCADE,
+        related_name='history'
+    )
+    old_content = models.TextField()
+    edit_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of Message ID {self.message.id}"
 
     def __str__(self):
         return f"Notification for {self.user.username} about message ID {self.message.id}"
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,11 +1,32 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
+    """
+    a signal that listens for new messages
+    """
     if created:
         Notification.objects.create(
             user=instance.receiver,
             message=instance
         )
+
+@receiver(pre_save, sender=Message)
+def log_edit_message(sender, instance, **kwargs):
+    """
+    Checking whether the instance
+    is in the database
+    """
+    if instance.pk:
+        try:
+            old_message = Message.objects.get(pk=instance.pk)
+            if old_message.content != instance.content:
+                MessageHistory.objects.create(
+                    message=instance,
+                    old_content=old_message.content
+                )
+                instance.edited = True
+        except Message.DoesNotExist:
+            pass
