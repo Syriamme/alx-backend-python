c1baa9f72083db92175ed122e94a04b265387bae
Syriamme <Roridaniel01@gmail.com>
2025-01-19 23:37:47 +0300

    Update asgi.py

--- Django-Middleware-0x03/messaging_app/asgi.py
+++ Django-Middleware-0x03/messaging_app/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from your_app_name.middleware import RequestLoggingMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
 
 # Get the default ASGI application
 django_asgi_app = get_asgi_application()
