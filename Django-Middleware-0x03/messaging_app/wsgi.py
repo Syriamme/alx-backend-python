9b3960c7dff35f3caa2e97008eb3408b8b0f6d63
Syriamme <Roridaniel01@gmail.com>
2025-01-19 23:38:14 +0300

    Update wsgi.py

--- Django-Middleware-0x03/messaging_app/wsgi.py
+++ Django-Middleware-0x03/messaging_app/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from your_app_name.middleware import RequestLoggingMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
 
 # Get the default WSGI application
 django_wsgi_app = get_wsgi_application()
