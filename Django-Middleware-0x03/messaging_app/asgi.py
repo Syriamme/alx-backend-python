f0552b5ad66f71621be2b30ea41498b91cbb331c
Syriamme <Roridaniel01@gmail.com>
2025-01-19 23:34:48 +0300

    Update asgi.py

--- Django-Middleware-0x03/messaging_app/asgi.py
+++ Django-Middleware-0x03/messaging_app/asgi.py
@@ -1,16 +1,11 @@
-"""
-ASGI config for messaging_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from your_app_name.middleware import RequestLoggingMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messaging_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
-application = get_asgi_application()
+# Get the default ASGI application
+django_asgi_app = get_asgi_application()
+
+# Wrap it with your middleware
+application = RequestLoggingMiddleware(django_asgi_app)
